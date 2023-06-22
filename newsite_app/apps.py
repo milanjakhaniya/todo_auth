fb31ed9e67441618063eba8cb48cc4a72a4bc938
Milan Jakhaniya <milanpatel1082@gmail.com>
2023-06-22 12:27:55 +0530

    Initial commit for todo with auth

---
+++ newsite_app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NewsiteAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'newsite_app'
+    
---
+++ newsite_app/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+# Add these imports at the top of the file
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+# ...
+# Add these new views at the bottom of the file
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('task_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('task_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
+# Add the login_required decorator to the CRUD views
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+@login_required
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'task_form.html', {'form': form})
+
+
+@login_required
+def task_update(request, id):
+    task = get_object_or_404(Task, pk=id)
+    if task.user != request.user:
+        return HttpResponseForbidden()
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'task_form.html', {'form': form})
+
+def task_delete(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    return redirect('task_list')
