e73651e96259ec1883efc2e30b457919b061aa71
Milan Jakhaniya <milanpatel1082@gmail.com>
2023-07-01 15:32:35 +0530

    added CRUD task using bootstrap

--- newsite/settings.py
+++ newsite/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'newsite_app',
-    'bootstrap5',
+    'bootstrap5', 
+    'fontawesomefree',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
