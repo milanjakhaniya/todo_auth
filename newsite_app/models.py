bc0951511e796dd90fa8528b2ac5bbe89a2d071c
Milan Jakhaniya <milanpatel1082@gmail.com>
2023-07-02 19:04:24 +0530

    breadcrumb added

--- newsite_app/models.py
+++ newsite_app/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200,default="")
     description = models.TextField()
     completed = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
